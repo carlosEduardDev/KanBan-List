bce77c8f705c6d69731f6cd371b53808dc29c0dc
Carlos <carlos8189.mk@gmail.com>
2024-04-24 18:55:47 -0300

    feat(table):implemented component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <main>
+      <Table>
+        <TableCaption>Kanban task list</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-center">Todo</TableHead>
+            <TableHead className="text-center">Doing</TableHead>
+            <TableHead className="text-center">Done</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="text-center">Exemple1</TableCell>
+            <TableCell className="text-center">Exemple2</TableCell>
+            <TableCell className="text-center">Exemple3</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
 };
 
 export default App;
