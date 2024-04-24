bec935c56809fe8d1a256fe50fd506d3b209294e
Carlos <carlos8189.mk@gmail.com>
2024-04-24 19:47:44 -0300

    style(form_task):flex and responsive adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import { Input } from "./components/ui/input";
 const App = () => {
   return (
     <main>
-      <header className="bg-gray-800 flex h-16 items-center text-slate-50 justify-between px-16 py-3">
-        <h1>Tasks - Carlos</h1>
-        <form className="flex space-x-2">
+      <header className="bg-gray-800 flex h-16 items-center text-slate-50 justify-between sm:px-16 px-5 py-3">
+        <h1 className="sm:block hidden">Tasks - Carlos</h1>
+        <form className="flex justify-between sm:w-80 w-full space-x-2">
           <Input className="text-gray-800 max-w-sm" type="text" placeholder="Task" />
           <Input className="text-gray-800 max-w-14 text-center" type="submit" value="Add" />
         </form>
