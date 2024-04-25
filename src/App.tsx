c1948486f44db87b6f0d0bcd7352d846f0227864
Carlos <carlos8189.mk@gmail.com>
2024-04-25 10:10:15 -0300

    feat(task object): add task object

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,27 @@ import {
 import { Input } from "./components/ui/input";
 
 const App = () => {
+  const tasks = {
+    todo: [],
+    doing: [],
+    done: [],
+  }
+
   return (
     <main>
       <header className="bg-gray-800 flex h-16 items-center text-slate-50 justify-between sm:px-16 px-5 py-3">
         <h1 className="sm:block hidden">Tasks - Carlos</h1>
         <form className="flex justify-between sm:w-80 w-full space-x-2">
-          <Input className="text-gray-800 max-w-sm" type="text" placeholder="Task" />
-          <Input className="text-gray-800 max-w-14 text-center" type="submit" value="Add" />
+          <Input
+            className="text-gray-800 max-w-sm"
+            type="text"
+            placeholder="Task"
+          />
+          <Input
+            className="text-gray-800 max-w-14 text-center"
+            type="submit"
+            value="Add"
+          />
         </form>
       </header>
       <Table>
